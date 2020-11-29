4a0968ebb5b3d6190e8add30693bbc0abf47d9bd
vijay <vijaybalajitheboss@gmail.com>
2020-11-29 13:21:16 +0530

    Deleting and checking medicine details added

--- src/Modules/Login.py
+++ src/Modules/Login.py
@@ -1,6 +1,7 @@
 import os
 import sqlite3
 from Modules.Colours import *
+from Modules.Login_functions import *
 
 def login(username):
     os.system('cls')
@@ -21,45 +22,54 @@ def login(username):
     except:
         pass
 
-    cyan("-" * 15)
-    print("""What would you like to do?
-    1) Add perscriptions
-    2) Modify perscriptions
-    3) Delete perscriptions
-    4) Set reminders for appointments
-    5) Exit""")
-    choice = int(input("> "))
-    cyan("-" * 15)
-
-    if(choice==1):
-        medication = input("Enter the name of your medicine \n> ").capitalize()
-        quantity = input("How many pills do you take at once? \n> ").capitalize()
-        temp = 0
-        print("""Time at which you are prescribed to take medications?
-        (Type in the numbers and hit enter. Once done, enter 5)
-        1) Morning
-        2) Afternoon
-        3) Night
+    while True:
+        cyan("-" * 15)
+        print("""What would you like to do?
+        1) Add perscriptions
+        2) Delete perscriptions
+        3) Set reminders for appointments
         4) Exit""")
-        time = ""
-        while (temp!=4):
-            temp = int(input("> "))
-            if(temp == 1):
-                if("Morning" in time):
-                    print("You have already added morning!")
+        choice = int(input("> "))
+        cyan("-" * 15)
+
+        if(choice==1):
+            medication = input("Enter the name of your medicine \n> ").capitalize()
+            quantity = input("How many pills do you take at once? \n> ").capitalize()
+            temp = 0
+            print("""Time at which you are prescribed to take medications?
+        (Type in the numbers and hit enter. Once done, enter 5)
+            1) Morning
+            2) Afternoon
+            3) Night
+            4) Exit""")
+            time = ""
+            while (temp!=4):
+                temp = int(input("> "))
+                if(temp == 1):
+                    if("Morning" in time):
+                        print("You have already added morning!")
+                    else:
+                        time = time + " Morning "
+                elif(temp == 2):
+                    if("Afternoon" in time):
+                        print("You have already added afternoon!")
+                    else:
+                        time = time + " Afternoon "
+                elif(temp == 3):
+                    if("Night" in time):
+                        print("You have already added Night!")
+                    else:
+                        time = time + " Night "
+                elif(temp == 4):
+                    print("You will be reminded to take your pills in the" + time)
                 else:
-                    time = time + " Morning "
-            elif(temp == 2):
-                if("Afternoon" in time):
-                    print("You have already added afternoon!")
-                else:
-                    time = time + " Afternoon "
-            elif(temp == 3):
-                if("Night" in time):
-                    print("You have already added Night!")
-                else:
-                    time = time + " Night "
-            elif(temp == 4):
-                print("You will be reminded to take your pills in the" + time)
+                    print("Type in numbers from 1 to 4 only")
+            
+            add_medications(username, medication, quantity, time)
+
+        if(choice == 2):
+            medication = input("Enter the name of the medicine you want to remove\n> ").capitalize()
+            if(check_medications(username, medication)):
+                delete_medications(username, medication)
             else:
-                print("Type in numbers from 1 to 4 only")
+                print("Medicine entered does not exist. Recheck the name entered")
---
+++ src/Modules/Login_functions.py
@@ -0,0 +1,24 @@
+from Modules.Login import *
+
+conn = sqlite3.connect('assets/Database.db')
+cursor = conn.cursor()
+
+def add_medications(username, name, quantity, time):
+    command = 'INSERT INTO {} (NAME,QUANTITY,TIME) VALUES("'.format(username)+name+'","'+quantity+'","'+time+'");'
+    conn.execute(command)
+    conn.commit()
+
+def check_medications(username, name):
+    command = ('SELECT NAME from {} where NAME = "'.format(username) + name + '"')
+    cursor.execute(command)
+    data = cursor.fetchall()
+    if len(data) == 0:
+        return False
+    else:
+        return True
+
+def delete_medications(username, name):
+    command = ('DELETE from {} where NAME = "'.format(username) + name + '"')
+    conn.execute(command)
+    conn.commit()
+    green("\n" + name + " details have been deleted from the database successfully.\n")
